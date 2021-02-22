b6a0fed4f7a3863e6bddeb65cbdf6e7e8643636a
akmanrisal <akmanrisal.r@ku.th>
2021-02-22 15:46:09 +0700

    Create lab4.c

---
+++ lab4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int x,i,j=2,a;
+
+    scanf("%d",&x);
+    if(x==1)
+        printf("1");
+
+    for(i=1; i<x; i++)
+    {
+        for(j=2; j<x; j++)
+        {
+            if(i%j==0)
+                break;
+        }
+        if(i==j)
+            a=i;
+    }
+    if(x!=1)
+        printf("%d",a);
+
+    return 0;
+}
+
