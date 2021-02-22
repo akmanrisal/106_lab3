ad68d59cae67f3dd45b8a3c82a2e05ee2e31668e
akmanrisal <akmanrisal.r@ku.th>
2021-02-22 15:55:26 +0700

    Initial commit

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int time[47];
+    int p,n,i;
+
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+       {
+        //time[i]=30;
+        // for(i=0;i<47;i++)
+            scanf("%d",&time[i]);
+            scanf("%d",&p);
+       }
+
+}
